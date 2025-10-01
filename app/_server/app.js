8f7e4d24238a0d3296c8d4e2ff4607d2f39b41d5
Arthur <art7cf@protonmail.com>
2025-10-01 20:44:51 +0300

    Update app configuration

--- app/_server/app.js
+++ app/_server/app.js
@@ -1,12 +1,27 @@
-import { createApp } from '@kottster/server';
+import { createApp, createIdentityProvider } from '@kottster/server';
 import schema from '../../kottster-app.json';
 
+/* 
+ * For security, consider moving the secret data to environment variables.
+ * See https://kottster.app/docs/deploying#before-you-deploy
+ */
 export const app = createApp({
   schema,
+  secretKey: '<your-secret-key-here>',
 
-  /* 
-  * For security, consider moving the secret key to an environment variable: 
-  * https://kottster.app/docs/deploying#before-you-deploy
-  */
-  secretKey: process.env.SECRET_KEY,
+  
+  /*
+   * The identity provider configuration.
+   * See https://kottster.app/docs/app-configuration/identity-provider
+   */
+  identityProvider: createIdentityProvider('sqlite', {
+    fileName: 'app.db',
+
+    passwordHashAlgorithm: 'bcrypt',
+    jwtSecretSalt: '<your-jwt-secret-salt-here>',
+    
+    /* The root admin user credentials */
+    rootUsername: 'admin',
+    rootPassword: 'admin',
+  }),
 });
